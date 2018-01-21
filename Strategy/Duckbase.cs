0fd3f84c716d6b4166a06adbbfade31e8761d10b
MikhailChaplygin <ChaplyginM@mail.ru>
2018-01-21 21:40:02 +0300

    Рабочая версия + реализация связи клиента и стратегии

---
+++ Strategy/Duckbase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    abstract class Duckbase : Iflyable, Iquackable
+    {
+        public virtual void Fly()
+        {
+            Console.WriteLine("I'm Flying");
+        }
+        public virtual void Quack()
+        {
+            Console.WriteLine("Quack!!!");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("I'm swimming");
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ Strategy/SimpleDuck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Strategy
+{
+    class SimpleDuck : Duckbase
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I'm a Simple Duck");
+        }
+
+        public override void Fly()
+        {
+
+        }
+    }
+}
